fd541a5276afe26ab26385a0d0ffc5ad0e7b8364
Nabyeol An <an1413@hs.ac.kr>
2022-07-10 23:41:47 +0900

    first commit

---
+++ json_002.js
@@ -0,0 +1,33 @@
+const person = [
+    {"name":"홍길동", "age":20, "nationality" :"대한민국"},
+    {"name":"안나별","age":21, "nationality":"대한민국"},
+    {"name":"슈바이처", "age":43,"nationality":"미국"},
+    {"name" : "징기스칸" , "age":34,"nationality":"몽골"}
+];
+
+console.log(typeof person[0]); 
+console.log(typeof person[1]); 
+
+
+console.log('---------------------------');
+console.log(person[3].name+ ""+ person[0].age +""+ person[0].nationality);
+
+//[3] : 반복
+console.log('---------------------------전개 연산자');
+const str1 = "korea";
+console.log(...person);
+console.log(...str1);   //전개 연산자
+console.log([...str1]);   //_proto __ --> Array
+console.log({...str1});   // _proto__ --> Object
+console.log({...person}[2].name);   //슈바이처
+
+console.log({...person});   // _proto__ --> Object
+
+// [4] : 반복 가능한 객체 --> for .. of... (전개 연산자)
+console.log('-----------------------------------for ..of');
+for(let ele of person){                     // person --> iterable 즉, 반복 가능한 객체가 오면 된다.
+    console.log(ele);
+
+}
+
+
